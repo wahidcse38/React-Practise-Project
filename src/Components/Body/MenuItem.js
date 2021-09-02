acf04a8081b525ba7fadddea694101e7183232b7
wahidcse38 <85180005+wahidcse38@users.noreply.github.com>
2021-09-01 15:04:26 -1200

    design menu component

--- src/Components/Body/MenuItem.js
+++ src/Components/Body/MenuItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardImgOverlay, CardTitle } from 'reactstrap';
 
 const MenuItem = (props) => {
     console.log(props)
     return (
         <div>
-
+            <Card style={{ margin: "10px 0" }}>
+                <CardBody>
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} />
+                    <CardTitle style={{ cursor: "pointer", textAlign: "center" }}>{props.dish.name}</CardTitle>
+                </CardBody>
+            </Card>
         </div>
     )
 }
