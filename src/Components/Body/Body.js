41a672e51d1dd4a32795d06352ccfa06c7b6b96e
wahidcse38 <85180005+wahidcse38@users.noreply.github.com>
2021-09-01 14:20:48 -1200

    Initial Setup and Component tree

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+function Body() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Body
---
+++ src/Components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Body from './Body/Body';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+
+function Main() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
+
+export default Main;
